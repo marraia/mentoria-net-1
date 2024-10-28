80799cddccb78463586a39e1bdc40cfd2864a412
Melissa Martins <melmartins.pro@gmail.com>
2024-10-28 11:09:12 -0300

    [ADD] -

--- Treinamento2/Program.cs
+++ Treinamento2/Program.cs
@@ -1,16 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
+﻿
 /*1- Escrever um algoritmo gerenciamento de usuários! 
 Especificações:
 Classe de usuários: CPF - Nome - Email - Data Nascimento -  DataCadastro
 
 Funcionalidades:
-    - Cadastrar um novo usuários na lista
-    - Visualizar todos os usuários cadastrados
-        - Mostrar a idade do usuários
+    - Cadastrar um novo usuários na lista v
+    - Visualizar todos os usuários cadastrados v
+        - Mostrar a idade do usuários v 
         - Quais anos do nascimento são números pares
     - Editar os dados do usuários
         - Pesquisar o usuários pelo CPF
@@ -25,3 +21,86 @@ Funcionalidades:
 
 */
 
+using System.Security.Cryptography;
+using Treinamento2;
+var usuario = new Usuario();
+bool sair = false;
+Console.WriteLine("=====================Bem vindo ao sistema de Cadastramento!=====================");
+Console.WriteLine("Nos diga se é sua 1º vez aqui:");
+Console.WriteLine("\tn - Primeira vez");
+Console.WriteLine("\tc - Já possuo cadastro");
+var novoUsuario = Console.ReadLine();
+
+if (novoUsuario == "n")
+{
+   
+        CadastroOP();
+    do
+    {
+        OpcoesCadastro();
+    } while (!sair);
+        
+   
+}/*else if( novoUsuario == "c")
+{
+    if(cpf == null)
+    {
+        Console.WriteLine("Desclpe,seu cpf não foi encontrado, tente novamente!");
+        Console.WriteLine("Digite seu cpf: ");
+        var cpf = Console.ReadLine();
+    }
+
+    //if()
+    {
+        Console.WriteLine("Digite seu cpf: ");
+        var cpf = Console.ReadLine();
+        Console.WriteLine($"Bem vindo de volta {cpf}, ");
+        Console.WriteLine("Suas opções: ");
+        Console.WriteLine("\tu - Visualizar usuarios cadastrados");
+        Console.WriteLine("\tc - Editar meu cadastro");
+        Console.WriteLine("\te - Sair");
+    }
+
+   
+}*/
+
+void CadastroOP()
+{
+    Console.WriteLine("Vamos fazer seu cadastro: ");
+    Console.WriteLine("Nos diga seu nome completo: ");
+    var nomeCompleto = Console.ReadLine();
+    Console.WriteLine($"Muito bem, {nomeCompleto}, agora nos diga seu CPF (sem hífen): ");
+    var cpf = Console.ReadLine();
+    Console.WriteLine("Email: ");
+    var email = Console.ReadLine();
+    Console.WriteLine("E por ultimo, sua data de nascimento (utilize o formato 00/00/00): ");
+    var datanasc = Convert.ToDateTime(Console.ReadLine());
+    usuario.AdicionarUsuario(cpf, nomeCompleto, email, datanasc);
+    Console.WriteLine($"Cadastro realizado com sucesso!");
+    Console.WriteLine(" A seguir suas opções: ");
+    Console.WriteLine("\tu - Visualizar usuarios cadastrados");
+    Console.WriteLine("\ta - Cadastrar novamente");
+    Console.WriteLine("\te - Sair");
+}
+void OpcoesCadastro()
+{
+    var userOP = Console.ReadLine();
+    switch (userOP)
+    {
+        case "u":
+            var exibir = usuario.ExibirUsuarios();
+            Console.WriteLine("===============Usuarios Cadastrados=============\n");
+            Console.WriteLine($"{exibir}");
+
+            Console.WriteLine("\ta - Cadastrar novamente");
+            Console.WriteLine("\te - Sair");
+            break;
+        case "a":
+            CadastroOP();
+            break;
+        case "e":
+            sair = true;
+            break;
+    }
+}
+ 
---
+++ Treinamento2/Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinamento2
+{
+    public class Usuario
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime Data { get; set; }
+        private List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+
+        
+        //adicionar usuarios
+       public void AdicionarUsuario(string cpf, string nome, string email, DateTime datanasc)
+        {
+            var usuario = new Usuario
+            {
+                Data = DateTime.Now,
+                CPF = cpf,
+                Nome = nome,
+                Email = email,
+                DataNasc = datanasc
+            };
+          Usuarios.Add(usuario);
+        }
+        //idade do usuario
+        public double Idade()
+        {
+           var Idade = Data.Year - DataNasc.Year;
+            return Idade;
+        }
+
+        //historico de usuarios
+        public string ExibirUsuarios()
+        {
+            var usuario = string.Empty;
+            foreach (var user in Usuarios)
+            {
+                usuario += $" Data de cadastro: {user.Data} | Cpf: {user.CPF} | Nome : {user.Nome} | Email: {user.Email} | Data de Nascimento: {user.DataNasc.Year} | Idade: {user.Idade()} anos \n" ;
+
+            }
+            return usuario;
+        }
+
+        
+    }
+
+    
+}
+//CPF - Nome - Email - Data Nascimento -  DataCadastro
